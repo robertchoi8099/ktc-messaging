98aba3cbd93444a0aaa0ae98228e62fc8798e2e9
Wil Reichert <wil.reichert@kt.com>
2013-07-25 00:09:37 +0900

    revert attribute to normal

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,12 +14,12 @@ end
 
 #node.default["openstack"]["mq"]["server_role"] = "ktc-messaging"
 # This attribute tells rabbit which interface to bind to
-node.override["openstack"]["mq"]["bind_interface"] = get_interface "management"
+node.default["openstack"]["mq"]["bind_interface"] = get_interface "management"
 
 # these attibutes are searched for by openstack-network and openstack-storage 
 # to find the rabbit instance 
-node.override["queue"]["host"] = get_interface_address "management"
-node.override["queue"]["port"] = "5672"
+node.default["queue"]["host"] = get_interface_address "management"
+node.default["queue"]["port"] = "5672"
 
 # necessary so other node can access those attributes
 node.save
