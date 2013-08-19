2d5c6f4d9d98a3621a65e63a78324c7c97b9ee56
Wil Reichert <wil.reichert@kt.com>
2013-08-20 00:08:30 +0900

    add etcd service discovery

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,8 @@ class ::Chef::Recipe
   include ::KTCUtils
 end
 
+d = get_openstack_service_template(get_interface_address("management"), "5672")
+register_service("rabbitmq", d)
 
 #node.default["openstack"]["mq"]["server_role"] = "ktc-messaging"
 # This attribute tells rabbit which interface to bind to
