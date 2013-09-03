15e0af122903aa45f0fd2464c866ba545f717386
Wil Reichert <wil.reichert@kt.com>
2013-09-03 16:42:16 +0900

    remote comment testing

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,35 +7,30 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#class ::Chef::Recipe
-#  include ::Openstack
-#end
+class ::Chef::Recipe
+  include ::KTCUtils
+end
 
-#class ::Chef::Recipe
-#  include ::KTCUtils
-#end
+d = get_openstack_service_template(get_interface_address("management"), "5672")
+register_member("rabbitmq", d)
 
-#d = get_openstack_service_template(get_interface_address("management"), "5672")
-puts "########## register member ################"
-#register_member("rabbitmq", d)
+iface = node.default["interface_mapping"]["management"]
 
-#iface = node.default["interface_mapping"]["management"]
-
-#node.default["openstack"]["mq"]["server_role"] = "ktc-messaging"
+node.default["openstack"]["mq"]["server_role"] = "ktc-messaging"
 # This attribute tells rabbit which interface to bind to
-#node.override["openstack"]["mq"]["bind_interface"] = iface
-#if node["ha_disabled"].nil?
-#  node.override["openstack"]["mq"]["cluster"] = "true"
-#end
+node.override["openstack"]["mq"]["bind_interface"] = iface
+if node["ha_disabled"].nil?
+  node.override["openstack"]["mq"]["cluster"] = "true"
+end
 
 # these attibutes are searched for by openstack-network and openstack-storage 
 # to find the rabbit instance 
-#node.default["queue"]["host"] = address_for iface
-#node.default["queue"]["port"] = "5672"
+node.default["queue"]["host"] = address_for iface
+node.default["queue"]["port"] = "5672"
 
 # necessary so other node can access those attributes via chef search
-#node.save
+node.save
 
-#include_recipe "openstack-common"
-#include_recipe "openstack-common::logging"
-#include_recipe "openstack-ops-messaging::server"
+include_recipe "openstack-common"
+include_recipe "openstack-common::logging"
+include_recipe "openstack-ops-messaging::server"
