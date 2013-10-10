2904b7a3a3bfb7d3175e696dfa83948c0104297c
Wil Reichert <wil.reichert@kt.com>
2013-10-10 12:15:38 +0900

    clean up kitchen

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description      'Installs/Configures ktc-messaging'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "ktc-utils", "~> 0.3.1"
+depends "ktc-utils", "> 0.3.1"
 depends "openstack-common", "~> 0.4.3"
 depends "openstack-ops-messaging", "~> 7.0.0"
-depends "services", "~> 1.0.6"
-depends "users", "~> 1.1.4"
+depends "services", "> 1.0.6"
+depends "users"
