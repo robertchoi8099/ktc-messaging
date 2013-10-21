b463194a1e0088f119d56bbb02e9d421fcf1b558
Jesse Nelson <spheromak@gmail.com>
2013-10-21 15:57:05 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'wil.reichert@kt.com'
 license          'All rights reserved'
 description      'Installs/Configures ktc-messaging'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends "ktc-utils", "> 0.3.1"
 depends "openstack-common", "~> 0.4.3"
