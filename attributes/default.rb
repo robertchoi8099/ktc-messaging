9b20df322ead01ec0ee59b7e7069f13dbde65e49
Robert <taeil.choi@kt.com>
2013-11-11 14:58:40 +0900

    add process monitoring

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
-#
+# process monitoring
+default["openstack"]["mq"]["mq_processes"] = [
+  { "name" => "rabbitmq-server", "shortname" => "rabbitmq-server" },
+  { "name" => "beam.smp", "shortname" => "beam.smp" }
+]
